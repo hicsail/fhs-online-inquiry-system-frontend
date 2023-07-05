c168b99939910246affb33dfd3a76dada873b8d0
wenhwang97 <wenhwang@bu.edu>
2023-07-05 16:02:26 -0400

    add sidebar

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,13 +1,21 @@
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
-import { FC } from 'react';
+import { AppBar, Box, Drawer, IconButton, List, ListItem, ListItemButton, Toolbar, Typography } from '@mui/material';
+import { FC, useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 
 export const Dashboard: FC = () => {
+  const [open, setOpen] = useState(false);
+
+  const datasets = [{ displayedName: 'Brain Data', name: 'brain_data' }];
+
+  // handlers for sidebar
+  const handleDrawerOpen = () => setOpen(true);
+  const handleDrawerClose = () => setOpen(false);
+
   return (
     <Box sx={{ display: 'flex' }}>
-      <AppBar component="nav" color="default">
+      <AppBar component="nav" color="default" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
         <Toolbar>
-          <IconButton>
+          <IconButton onClick={handleDrawerOpen}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div">
@@ -15,6 +23,16 @@ export const Dashboard: FC = () => {
           </Typography>
         </Toolbar>
       </AppBar>
+      <Drawer variant="temporary" anchor="left" open={open} onClose={handleDrawerClose}>
+        <Toolbar />
+        <List>
+          {datasets.map((dataset) => (
+            <ListItem key={dataset.name} disablePadding>
+              <ListItemButton>{dataset.displayedName}</ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </Box>
   );
 };
