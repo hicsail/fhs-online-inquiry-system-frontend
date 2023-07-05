de3278e8fa71f96d77dd7bedd2c24f988c6a9d65
Cody Karjadi <ckarjadi@bu.edu>
2023-07-05 11:36:46 -0400

    resolved initial slider issues due to missing @emotion/styled library.

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
-import Slider from './slider';
+import Demo from './r_slider';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,7 +26,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-        <Slider />
+        <Demo />
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
